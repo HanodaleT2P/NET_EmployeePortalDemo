cc5825c9097745df0c37633be13825d825a81ef0
indirah80 <113572004+indirah80@users.noreply.github.com>
2025-06-15 09:48:28 +0800

    Added separate department service

---
+++ Interfaces/IDepartmentEFService.cs
@@ -0,0 +1,15 @@
+﻿using EmployeePortal.Models;
+using EmployeePortalDemo.Models;
+using EmployeePortalDemo.ViewModels;
+
+
+
+namespace EmployeePortalDemo.Interfaces
+{
+    public interface IDepartmentEFService
+    { 
+     
+        Task<List<DepartmentViewModel>> GetDepartmentsAsync();
+       
+    }
+}
---
+++ Services/DepartmentEFService.cs
@@ -0,0 +1,41 @@
+﻿
+using EmployeePortal.Models;
+using EmployeePortalDemo.Interfaces;
+using EmployeePortalDemo.Models;
+using EmployeePortalDemo.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+
+namespace EmployeePortalDemo.Services
+{
+    public class DepartmentEFService : IDepartmentEFService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DepartmentEFService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+       
+        public async Task<List<DepartmentViewModel>> GetDepartmentsAsync()
+        {
+            return await _context.Departments
+                .Include(e => e.Employees)
+                .Select(e => new DepartmentViewModel
+                {
+                    Department = new Department
+                    {
+                        DepartmentId = e.DepartmentId,
+                        Name = e.Name,
+                        LocationId = e.LocationId
+                    },
+                   
+                })
+                .ToListAsync();
+        }
+
+    }
+
+}
